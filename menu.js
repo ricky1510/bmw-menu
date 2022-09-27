fd3dab75540a44667bc1557719228b3a9491b125
Ricky Admiraal <ricky@Rickys-MacBook-Pro.local>
2022-09-27 10:21:18 +0200

    BMW menu

--- menu.js
+++ menu.js
@@ -1,32 +1,38 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-	const tl = gsap.timeline();
+	const mainTimeline = gsap.timeline();
+	const altTimeline = gsap.timeline();
 	const mainMenu = document.querySelector('.main-menu');
 	const mainMenuLinks = document.querySelectorAll('.main-menu a');
 	const mainOverlayLinks = document.querySelectorAll('.main-overlay-link');
 	const closeBtn = document.querySelector('.close-btn');
 	const backDrop = document.querySelector('.menu-overlay-backdrop');
+	const toggleMenu = document.querySelector('.toggle-menu');
 
 	function activateSubmenu(link) {
-		console.log('switchSubmenu');
 		const target = link.dataset.target;
 
 		const oldActiveSubmenu = document.querySelector('.submenu.active');
 		if (oldActiveSubmenu) {
 			oldActiveSubmenu.classList.remove('active');
+			altTimeline.to(oldActiveSubmenu, {height: 0, duration: .2, ease: 'power1.in'});
 		}
 
 		const activeSubmenu = document.querySelector(`.submenu[data-submenu="${target}"]`)
 		activeSubmenu.classList.add('active');
-		console.log('active', activeSubmenu);
 
-		tl.set('.submenu a', {opacity: 0})
-		tl.fromTo(`.submenu[data-submenu="${target}"] a`, {y: -80}, {y: 0, duration: .3, ease: 'power1.in', stagger: .05}, '<');
-		tl.fromTo(`.submenu[data-submenu="${target}"] a`, {opacity: 0}, {opacity: 1, duration: .3, ease: 'power1.in', stagger: .05}, '<.2');
+		const scrollHeight = document.querySelector(`.submenu[data-submenu="${target}"]`).scrollHeight;
+		mainTimeline.set('.submenu a', {opacity: 0});
+		console.log('activate submenu');
+		mainTimeline.fromTo(`.submenu[data-submenu="${target}"]`, {height: 0}, {height: scrollHeight, duration: .2, ease: 'power1.in'});
+		mainTimeline.fromTo(`.submenu[data-submenu="${target}"] a`, {y: -80}, {y: 0, duration: .3, ease: 'power1.in', stagger: .05});
+		mainTimeline.fromTo(`.submenu[data-submenu="${target}"] a`, {opacity: 0}, {opacity: 1, duration: .3, ease: 'power1.in', stagger: .05}, '<.2');
 	}
 
 	function deactivateOverlayLink() {
 		const activeOverlayLink = document.querySelector(`.main-overlay-link.active`);
-		activeOverlayLink.classList.remove('active');
+		if (activeOverlayLink) {
+			activeOverlayLink.classList.remove('active');
+		}
 	}
 
 	function activateOverlayLink(link) {
@@ -39,28 +45,16 @@ window.addEventListener('DOMContentLoaded', (event) => {
 	function closeMenu(link) {
 		document.querySelector('body').classList.remove('menu-open');
 
-		tl.to('.menu-overlay', {x: '-100%', duration: .3, ease: 'power2.in'});
-		tl.to('.menu-overlay-backdrop', {opacity: 0, duration: .3, ease: 'power2.out'});
+		mainTimeline.to('.menu-overlay', {x: '-100%', duration: .3, ease: 'power2.in'});
+		mainTimeline.to('.menu-overlay-backdrop', {opacity: 0, duration: .3, ease: 'power2.out'});
 	}
 
-	function openMenu(link) {
-		const target = link.dataset.target;
-		const oldActiveSubmenu = document.querySelector('.submenu.active');
-		if (oldActiveSubmenu) {
-			oldActiveSubmenu.classList.remove('active');
-		}
-
-		const activeSubmenu = document.querySelector(`.submenu[data-submenu="${target}"]`);
-		activeSubmenu.classList.add('active');
-
+	function openMenu() {
 		document.querySelector('body').classList.add('menu-open');
 
-		tl.to('.submenu a', {opacity: 0});
-		tl.fromTo('.menu-overlay-backdrop', {opacity: 0}, {opacity: 3, duration: .35, ease: 'power2.out'});
-		tl.to('.menu-overlay', {x: 0, duration: .35, ease: 'power2.out'}, '<');
-		tl.fromTo('.main-overlay-link', {opacity: .2}, {opacity: 1, duration: .4, ease: 'power4.in'}, '<');
-		tl.fromTo(`.submenu[data-submenu="${target}"] a`, {y: -80}, {y: 0, duration: .3, ease: 'power1.in', stagger: .05}, '<');
-		tl.fromTo(`.submenu[data-submenu="${target}"] a`, {opacity: 0}, {opacity: 1, duration: .3, ease: 'power1.in', stagger: .05}, '<.2');
+		mainTimeline.fromTo('.menu-overlay-backdrop', {opacity: 0}, {opacity: 3, duration: .6, ease: 'power2.out'});
+		mainTimeline.to('.menu-overlay', {x: 0, duration: .35, ease: 'power2.out'}, '<');
+		mainTimeline.fromTo('.main-overlay-link', {opacity: .2}, {opacity: 1, duration: .4, ease: 'power4.in'}, '<');
 	}
 
 	mainMenuLinks.forEach((link) => {
@@ -70,7 +64,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 			const link = event.target;
 
 			activateOverlayLink(link);
-			openMenu(link);
+			openMenu();
+			activateSubmenu(link);
 		});
 	});
 
@@ -84,6 +79,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 			activateOverlayLink(link);
 			activateSubmenu(link);
 		});
+	});
+
+	toggleMenu.addEventListener('click', () => {
+		openMenu();
 	});
 
 	closeBtn.addEventListener('click', (event) => {
